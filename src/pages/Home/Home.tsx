57cf13fb151f77343293662ee459318ae019ed8d
Kevin Freistroffer <Kevin.freistroffer@gmail.com>
2023-09-04 08:55:56 -0700

    theme

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,37 +3,52 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { styled } from "styled-components";
 import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
+import Typography from "@mui/material/Typography";
 
 import {
   StyledAlgorithmName,
-  StyledCaption,
+  StyledTitle,
   StyledIcon,
   StyledOverlay,
   StyledSelection,
   StyledSelectionContainer,
 } from "./styled";
+import { Link } from "react-router-dom";
 
 export const Home = (): JSX.Element => {
   return (
     <PageLayout data-testid="home-page">
-      <StyledCaption>Select an algorithm </StyledCaption>
-      <StyledSelectionContainer>
+      <Typography variant="h2" gutterBottom>
+        Select an algorithm
+      </Typography>
+      <div>
+        <Button
+          size="large"
+          variant="outlined"
+          sx={{ mr: 1 }}
+          color="secondary"
+        >
+          <Link to="/linear-regression">Linear Regression</Link>
+        </Button>
+        <Button size="large" variant="outlined" sx={{ mr: 1 }}>
+          <Link to="/logistic-regression">Logistic Regression</Link>
+        </Button>
+      </div>
+      {/* <StyledSelectionContainer>
         <StyledOverlay />
-        {/* Linear Regression */}
         <StyledSelection>
           <StyledIcon>
             <EmojiObjectsIcon />
           </StyledIcon>
           <StyledAlgorithmName>Linear Regression</StyledAlgorithmName>
         </StyledSelection>
-        {/* Logistic Regression */}
         <StyledSelection>
           <StyledIcon>
             <EmojiObjectsIcon />
           </StyledIcon>
           <StyledAlgorithmName>Logistic Regression</StyledAlgorithmName>
         </StyledSelection>
-      </StyledSelectionContainer>
+      </StyledSelectionContainer> */}
     </PageLayout>
   );
 };
